4793461b27443bac6bad60744d5ccc074d47b595
Jacco Meijer <jacco@jaccomeijer.nl>
2024-02-15 19:59:18 +0100

    chore: update ui library

--- src/data/globals.js
+++ src/data/globals.js
@@ -1,23 +1,9 @@
 import { config } from './config.js'
+import { metadata } from './metadata.js'
 
 export const globals = {
   assetUrl: config.assetUrl,
   baseUrl: config.baseUrl,
   imageSizes: config.imageSizes,
-  metadata: {
-    author: {
-      name: 'Jacco Meijer',
-      email: 'jacco@jaccomeijer.nl',
-      url: 'https://jaccomeijer.nl',
-    },
-    branding: 'Green Reference',
-    copyright: 'Copyright 2024 © Jacco Meijer',
-    description: 'A reference implementation of the Green UI Library.',
-    generator: 'esbuild',
-    language: 'en',
-    title: 'Green UI Library',
-    url: 'https://github.com/jaccomeijer/green-lib',
-    vendor: 'Made with Green UI Library',
-    version: '4.0.0',
-  },
+  metadata,
 }
