d9de717822463bf2f6518f85623fd5dcb43d014b
Jacco Meijer <jacco@jaccomeijer.nl>
2024-02-15 09:56:24 +0100

    refactor: central config

--- pages.js
+++ pages.js
@@ -1,16 +1,5 @@
-import { globals } from './src/data/globals.js'
+import { config } from './src/data/config.js'
 import { build } from '@jaccomeijer/green-build'
-
-const config = {
-  assetUrlPrefix: globals.assetUrlPrefix,
-  entryPointsGlob: 'src/pages/**/*.{mdx}',
-  imageSizes: globals.imageSizes,
-  initialProps: {},
-  outdir: 'docs',
-  removeBundle: false,
-  serve: false,
-  stripFromOutputPath: 'src/pages',
-}
 
 if (process.argv[2] === 'serve') {
   config.serve = true
---
+++ src/data/config.js
@@ -0,0 +1,14 @@
+export const config = {
+  assetUrlPrefix: '/green-reference/assets',
+  entryPointsGlob: 'src/pages/**/*.{mdx}',
+  imageSizes: {
+    s: 300,
+    m: 700,
+    l: 1000,
+  },
+  initialProps: {},
+  outdir: 'docs',
+  removeBundle: false,
+  serve: false,
+  stripFromOutputPath: 'src/pages',
+}
