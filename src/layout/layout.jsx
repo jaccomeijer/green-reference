ab05c86209ad57c4751302790e20ba0df1d772dc
Jacco Meijer <jacco@jaccomeijer.nl>
2024-03-04 21:25:41 +0100

    chore: setup index page

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -22,73 +22,35 @@ export const Layout = props => {
       <body>
         <a class="skip-link" href="#main-content">Skip to content</a>
         <header role="banner" class="site-header s-wrapper">
-          <div class="main-menu s-flex-row u-justify-space-between s-block-space u-block-space-2xs">
-            <nav aria-label="primary">
-              <NavigationHeader menu={mainMenu} class="s-flex-row" />
-            </nav>
-            <div class="site-branding">
-              Green lib
-            </div>
-          </div>
-          <nav class="sub-menu s-flex-row s-block-space u-block-space-3xs">
-            <Menu menu={subMenu} class="s-flex-row" />
-          </nav>
-        </header>
-        <main tabindex="-1" id="main-content">
-          <div class="page s-wrapper s-block-space u-block-space-l">
-            <article class="markdown s-flex-column u-gap-m">
-              {props.children}
-            </article>
-          </div>
-        </main>
-        <footer class="s-wrapper u-font-size--1">
-          This is the footer
-        </footer>
-      </body>
-    </html>
-  )
-
-
-  return (
-    <html lang={globals.metadata.content.language}>
-      <Head
-        enableReload={process.env.MODE === 'serve'}
-        favicon={favicon}
-        globals={globals}
-        page={props.page}
-        stylesheets={[settingsCss, globalCss]}
-      />
-      <body>
-        <div class="border-bottom">
-        <div class="container">
-        <HeaderNavigation
-          callActions={globals.actions.call}
-          globals={globals}
-          mainActions={mainActions}
-          pageUrl={props.page.url}
-          socialActions={globals.actions.social}
-        />
-        </div>
-        </div>
-        <div class="container">
-          {props.children}
-        </div>
-        <div class="border-top margin-top-6">
-          <div class="container">
-            <FooterNavigation 
+          <div class="s-block-space u-block-space-xs">
+            <NavigationHeader
+              callActions={globals.actions.call}
               globals={globals}
-              pageUrl={props.page.url}
+              mainActions={mainActions}
               socialActions={globals.actions.social}
             />
           </div>
-        </div>
-        <div class="border-top">
-          <div class="container">
-            <LegalNavigation
+        </header>
+        <main tabindex="-1" id="main-content" class="s-wrapper">
+          <article class="markdown s-flex-column u-gap-m">
+            {props.children}
+          </article>
+        </main>
+        <footer class="site-footer s-wrapper s-block-space u-block-space-l">
+          <div class="s-block-space u-block-space-2xs u-border-top">
+            <NavigationFooter
+              globals={globals}
+              pageUrl={props.page.url}
+              mainActions={mainActions}
+              socialActions={globals.actions.social}
+            />
+          </div>
+          <div class="s-block-space u-block-space-2xs u-border-top">
+            <NavigationLegal
               globals={globals}
             />
           </div>
-        </div>
+        </footer>
       </body>
     </html>
   )
