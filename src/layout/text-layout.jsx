420a15631c8e650bba30e09954f1271500200204
Jacco Meijer <jacco@jaccomeijer.nl>
2024-03-24 23:40:30 +0100

    chore: linter fix

--- src/layout/text-layout.jsx
+++ src/layout/text-layout.jsx
@@ -1,13 +1,11 @@
 import { BaseLayout } from './base-layout.jsx'
 
-export const TextLayout = props => {
-  return (
-    <BaseLayout {...props}>
-      <div className="s-wrapper" style={{'--wrapper-width': '45rem'}}>
-        <article className="markdown s-flex-column u-gap-0">
-          {props.children}
-        </article>
-      </div>
-    </BaseLayout>
-  )
-}
+export const TextLayout = props => (
+  <BaseLayout {...props}>
+    <div className="s-wrapper" style={{ '--wrapper-width': '45rem' }}>
+      <article className="markdown s-flex-column u-gap-0">
+        {props.children}
+      </article>
+    </div>
+  </BaseLayout>
+)
